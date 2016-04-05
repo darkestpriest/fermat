480f229a9b5f9cebc36ec956de7197aa76b57fbb
alexanderejm <alex_jimenez76@hotmail.com>
2016-04-05 02:36:05 -0430

    #5531 updated PluginRoot to reflect changes in interface

--- ART/plugin/sub_app_module/fermat-art-plugin-sub-app-module-artist-community-bitdubai/src/main/java/com/bitdubai/fermat_art_plugin/layer/sub_app_module/artist_community/developer/bitdubai/version_1/ArtistCommunityPluginRoot.java
+++ ART/plugin/sub_app_module/fermat-art-plugin-sub-app-module-artist-community-bitdubai/src/main/java/com/bitdubai/fermat_art_plugin/layer/sub_app_module/artist_community/developer/bitdubai/version_1/ArtistCommunityPluginRoot.java
@@ -10,9 +10,16 @@ import com.bitdubai.fermat_api.layer.all_definition.enums.Layers;
 import com.bitdubai.fermat_api.layer.all_definition.enums.Platforms;
 import com.bitdubai.fermat_api.layer.all_definition.enums.Plugins;
 import com.bitdubai.fermat_api.layer.all_definition.enums.ServiceStatus;
+import com.bitdubai.fermat_api.layer.all_definition.settings.structure.SettingsManager;
 import com.bitdubai.fermat_api.layer.all_definition.util.Version;
-import com.bitdubai.fermat_api.layer.modules.interfaces.ModuleManager;
+import com.bitdubai.fermat_api.layer.modules.common_classes.ActiveActorIdentityInformation;
+import com.bitdubai.fermat_api.layer.modules.exceptions.ActorIdentityNotSelectedException;
+import com.bitdubai.fermat_api.layer.modules.exceptions.CantGetSelectedActorIdentityException;
+import com.bitdubai.fermat_api.layer.osa_android.file_system.PluginFileSystem;
 import com.bitdubai.fermat_art_api.layer.actor_connection.artist.interfaces.ArtistActorConnectionManager;
+import com.bitdubai.fermat_art_api.layer.sub_app_module.community.artist.ArtistCommunityPreferenceSettings;
+import com.bitdubai.fermat_art_api.layer.sub_app_module.community.artist.interfaces.ArtArtistCommunityManager;
+import com.bitdubai.fermat_art_api.layer.sub_app_module.community.artist.interfaces.ArtArtistCommunityManagerModule;
 import com.bitdubai.fermat_art_plugin.layer.sub_app_module.artist_community.developer.bitdubai.version_1.structure.ArtistCommunityManager;
 import com.bitdubai.fermat_pip_api.layer.platform_service.error_manager.enums.UnexpectedPluginExceptionSeverity;
 import com.bitdubai.fermat_pip_api.layer.platform_service.error_manager.interfaces.ErrorManager;
@@ -20,13 +27,18 @@ import com.bitdubai.fermat_pip_api.layer.platform_service.error_manager.interfac
 /**
  * Created by Manuel Perez (darkpriestrelative@gmail.com) on 08/03/16.
  */
-public class ArtistCommunityPluginRoot extends AbstractPlugin {
+public class ArtistCommunityPluginRoot extends AbstractPlugin implements ArtArtistCommunityManagerModule {
     @NeededAddonReference(platform = Platforms.PLUG_INS_PLATFORM, layer = Layers.PLATFORM_SERVICE, addon = Addons.ERROR_MANAGER)
     private ErrorManager errorManager;
 
     @NeededPluginReference(platform = Platforms.ART_PLATFORM, layer = Layers.IDENTITY,plugin = Plugins.ARTIST_ACTOR_CONNECTION)
     private ArtistActorConnectionManager artistCommunityModuleManager;
 
+    @NeededAddonReference(platform = Platforms.OPERATIVE_SYSTEM_API, layer = Layers.SYSTEM, addon = Addons.PLUGIN_FILE_SYSTEM)
+    PluginFileSystem pluginFileSystem;
+
+    private SettingsManager<ArtistCommunityPreferenceSettings> settingsManager;
+    private ArtArtistCommunityManager artistCommunityManagerinterface;
     private ArtistCommunityManager artistCommunityManager;
     /**
      * Default constructor
@@ -41,14 +53,12 @@ public class ArtistCommunityPluginRoot extends AbstractPlugin {
                 ,artistCommunityModuleManager
         );
     }
-    public ModuleManager getManager(){
-        return this.artistCommunityManager;
-    }
 
     @Override
     public void start() throws CantStartPluginException {
         try{
             initPluginManager();
+            System.out.println("########### ArtistCommunityPluginRoot");
         } catch (Exception e) {
             this.errorManager.reportUnexpectedPluginException(
                     Plugins.ARTIST_COMMUNITY_SUB_APP_MODULE,
@@ -66,5 +76,53 @@ public class ArtistCommunityPluginRoot extends AbstractPlugin {
     }
     public void stop(){
         this.serviceStatus = ServiceStatus.STOPPED;
+    }
+
+    /**
+     * This method is used by the fermat-core to get the plugin manager in execution time.
+     * @return
+     */
+
+    @Override
+    public ArtArtistCommunityManager getArtistCommunityManager() {
+        if(artistCommunityManagerinterface==null){
+            artistCommunityManagerinterface=new ArtistCommunityManager(
+                    errorManager,
+                    artistCommunityModuleManager
+                    );
+        }
+        return artistCommunityManagerinterface;    }
+
+    @Override
+    public SettingsManager<ArtistCommunityPreferenceSettings> getSettingsManager() {
+        if (this.settingsManager != null)
+            return this.settingsManager;
+
+        this.settingsManager = new SettingsManager<>(
+                pluginFileSystem,
+                pluginId
+        );
+
+        return this.settingsManager;
+    }
+
+    @Override
+    public ActiveActorIdentityInformation getSelectedActorIdentity() throws CantGetSelectedActorIdentityException, ActorIdentityNotSelectedException {
+        return null;
+    }
+
+    @Override
+    public void createIdentity(String name, String phrase, byte[] profile_img) throws Exception {
+
+    }
+
+    @Override
+    public void setAppPublicKey(String publicKey) {
+
+    }
+
+    @Override
+    public int[] getMenuNotifications() {
+        return new int[0];
     }
 }
