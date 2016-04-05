60cca315ec176e96bb2cd4b3a5ae0c3dda965b14
alexanderejm <alex_jimenez76@hotmail.com>
2016-04-05 02:36:42 -0430

    #5534 #5535 updated Manager to reflect changes made to the interface

--- ART/plugin/sub_app_module/fermat-art-plugin-sub-app-module-artist-community-bitdubai/src/main/java/com/bitdubai/fermat_art_plugin/layer/sub_app_module/artist_community/developer/bitdubai/version_1/structure/ArtistCommunityManager.java
+++ ART/plugin/sub_app_module/fermat-art-plugin-sub-app-module-artist-community-bitdubai/src/main/java/com/bitdubai/fermat_art_plugin/layer/sub_app_module/artist_community/developer/bitdubai/version_1/structure/ArtistCommunityManager.java
@@ -11,13 +11,9 @@ import com.bitdubai.fermat_api.layer.actor_connection.common.exceptions.Unexpect
 import com.bitdubai.fermat_api.layer.actor_connection.common.exceptions.UnsupportedActorTypeException;
 import com.bitdubai.fermat_api.layer.actor_connection.common.structure_abstract_classes.ActorConnectionSearch;
 import com.bitdubai.fermat_api.layer.actor_connection.common.structure_common_classes.ActorIdentityInformation;
-import com.bitdubai.fermat_api.layer.all_definition.settings.structure.SettingsManager;
-import com.bitdubai.fermat_api.layer.modules.common_classes.ActiveActorIdentityInformation;
-import com.bitdubai.fermat_api.layer.modules.exceptions.ActorIdentityNotSelectedException;
-import com.bitdubai.fermat_api.layer.modules.exceptions.CantGetSelectedActorIdentityException;
 import com.bitdubai.fermat_art_api.layer.actor_connection.artist.interfaces.ArtistActorConnectionManager;
 import com.bitdubai.fermat_art_api.layer.actor_connection.artist.utils.ArtistLinkedActorIdentity;
-import com.bitdubai.fermat_art_api.layer.sub_app_module.community.ArtistCommunityModuleManager;
+import com.bitdubai.fermat_art_api.layer.sub_app_module.community.artist.interfaces.ArtArtistCommunityManager;
 import com.bitdubai.fermat_pip_api.layer.platform_service.error_manager.interfaces.ErrorManager;
 
 import java.util.UUID;
@@ -25,7 +21,7 @@ import java.util.UUID;
 /**
  * Created by Alexander Jimenez (alex_jimenez76@hotmail.com) on 3/23/16.
  */
-public class ArtistCommunityManager implements ArtistCommunityModuleManager {
+public class ArtistCommunityManager implements ArtArtistCommunityManager {
     private final ErrorManager errorManager;
     private final ArtistActorConnectionManager artistActorConnectionManager;
     public ArtistCommunityManager(ErrorManager errorManager
@@ -64,30 +60,5 @@ public class ArtistCommunityManager implements ArtistCommunityModuleManager {
     @Override
     public void acceptConnection(UUID connectionId) throws CantAcceptActorConnectionRequestException, ActorConnectionNotFoundException, UnexpectedConnectionStateException {
         artistActorConnectionManager.acceptConnection(connectionId);
-    }
-
-    @Override
-    public SettingsManager getSettingsManager() {
-        return null;
-    }
-
-    @Override
-    public ActiveActorIdentityInformation getSelectedActorIdentity() throws CantGetSelectedActorIdentityException, ActorIdentityNotSelectedException {
-        return null;
-    }
-
-    @Override
-    public void createIdentity(String name, String phrase, byte[] profile_img) throws Exception {
-
-    }
-
-    @Override
-    public void setAppPublicKey(String publicKey) {
-
-    }
-
-    @Override
-    public int[] getMenuNotifications() {
-        return new int[0];
     }
 }
