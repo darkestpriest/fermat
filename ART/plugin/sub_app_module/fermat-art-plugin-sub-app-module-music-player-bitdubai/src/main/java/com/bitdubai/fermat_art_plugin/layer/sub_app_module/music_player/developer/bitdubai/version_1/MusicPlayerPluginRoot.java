61bda80ca98b6702544df1f6d8eddca38dafc105
alexanderejm <alex_jimenez76@hotmail.com>
2016-04-05 02:31:07 -0430

    #5596 pluginRoot Updated

--- ART/plugin/sub_app_module/fermat-art-plugin-sub-app-module-music-player-bitdubai/src/main/java/com/bitdubai/fermat_art_plugin/layer/sub_app_module/music_player/developer/bitdubai/version_1/MusicPlayerPluginRoot.java
+++ ART/plugin/sub_app_module/fermat-art-plugin-sub-app-module-music-player-bitdubai/src/main/java/com/bitdubai/fermat_art_plugin/layer/sub_app_module/music_player/developer/bitdubai/version_1/MusicPlayerPluginRoot.java
@@ -10,8 +10,16 @@ import com.bitdubai.fermat_api.layer.all_definition.enums.Layers;
 import com.bitdubai.fermat_api.layer.all_definition.enums.Platforms;
 import com.bitdubai.fermat_api.layer.all_definition.enums.Plugins;
 import com.bitdubai.fermat_api.layer.all_definition.enums.ServiceStatus;
+import com.bitdubai.fermat_api.layer.all_definition.settings.structure.SettingsManager;
 import com.bitdubai.fermat_api.layer.all_definition.util.Version;
+import com.bitdubai.fermat_api.layer.modules.common_classes.ActiveActorIdentityInformation;
+import com.bitdubai.fermat_api.layer.modules.exceptions.ActorIdentityNotSelectedException;
+import com.bitdubai.fermat_api.layer.modules.exceptions.CantGetSelectedActorIdentityException;
 import com.bitdubai.fermat_api.layer.modules.interfaces.ModuleManager;
+import com.bitdubai.fermat_api.layer.osa_android.file_system.PluginFileSystem;
+import com.bitdubai.fermat_art_api.layer.sub_app_module.music_player.MusicPlayerPreferenceSettings;
+import com.bitdubai.fermat_art_api.layer.sub_app_module.music_player.interfaces.ArtMusicPlayerManager;
+import com.bitdubai.fermat_art_api.layer.sub_app_module.music_player.interfaces.ArtMusicPlayerManagerModule;
 import com.bitdubai.fermat_art_plugin.layer.sub_app_module.music_player.developer.bitdubai.version_1.structure.MusicPlayerManager;
 import com.bitdubai.fermat_pip_api.layer.platform_service.error_manager.enums.UnexpectedPluginExceptionSeverity;
 import com.bitdubai.fermat_pip_api.layer.platform_service.error_manager.interfaces.ErrorManager;
@@ -20,7 +28,7 @@ import com.bitdubai.fermat_tky_api.layer.song_wallet.interfaces.SongWalletTokenl
 /**
  * Created by Manuel Perez (darkpriestrelative@gmail.com) on 29/03/16.
  */
-public class MusicPlayerPluginRoot extends AbstractPlugin {
+public class MusicPlayerPluginRoot extends AbstractPlugin implements ArtMusicPlayerManagerModule{
 
     @NeededAddonReference(platform = Platforms.PLUG_INS_PLATFORM, layer = Layers.PLATFORM_SERVICE, addon = Addons.ERROR_MANAGER)
     private ErrorManager errorManager;
@@ -28,7 +36,12 @@ public class MusicPlayerPluginRoot extends AbstractPlugin {
     @NeededPluginReference(platform = Platforms.ART_PLATFORM, layer = Layers.IDENTITY,plugin = Plugins.MUSIC_PLAYER_SUB_APP_MODULE)
     private SongWalletTokenlyManager songWalletTokenlyManager;
 
+    @NeededAddonReference(platform = Platforms.OPERATIVE_SYSTEM_API, layer = Layers.SYSTEM, addon = Addons.PLUGIN_FILE_SYSTEM)
+    PluginFileSystem pluginFileSystem;
+
     private MusicPlayerManager musicPlayerManager;
+    private ArtMusicPlayerManager artMusicPlayerManager;
+    private SettingsManager<MusicPlayerPreferenceSettings> settingsManager;
     /**
      * Default constructor
      */
@@ -39,32 +52,75 @@ public class MusicPlayerPluginRoot extends AbstractPlugin {
     private void initPluginManager(){
         this.musicPlayerManager = new MusicPlayerManager(errorManager,songWalletTokenlyManager);
     }
-    public ModuleManager getManager(){
-        return this.musicPlayerManager;
-    }
 
     @Override
     public void start() throws CantStartPluginException {
         try{
-            System.out.println("ART MUSIC PLAYER STARTED2");
             initPluginManager();
+            System.out.println("ART MUSIC PLAYER STARTED2");
         } catch (Exception e) {
             this.errorManager.reportUnexpectedPluginException(
-                    Plugins.ART_FAN_SUB_APP_MODULE,
+                    Plugins.MUSIC_PLAYER_SUB_APP_MODULE,
                     UnexpectedPluginExceptionSeverity.DISABLES_THIS_PLUGIN,
                     e);
             throw new CantStartPluginException(
                     CantStartPluginException.DEFAULT_MESSAGE,
-                    e, "Cant start Sub App Musci Player Module plugin.",
+                    e, "Cant start Sub App Music Player Module plugin.",
                     null);
         }
         /**
          * nothing left to do.
          */
-        System.out.println("ART MUSIC PLAYER STARTED");
         this.serviceStatus = ServiceStatus.STARTED;
     }
     public void stop(){
         this.serviceStatus = ServiceStatus.STOPPED;
+    }
+
+    /**
+     * This method is used by the fermat-core to get the plugin manager in execution time.
+     * @return
+     */
+    @Override
+    public ArtMusicPlayerManager getMusicPlayerManager() {
+        if(artMusicPlayerManager==null){
+            artMusicPlayerManager=new MusicPlayerManager(
+                    errorManager,
+                    songWalletTokenlyManager
+                    );
+        }
+        return artMusicPlayerManager;    }
+
+    @Override
+    public SettingsManager<MusicPlayerPreferenceSettings> getSettingsManager() {
+        if (this.settingsManager != null)
+            return this.settingsManager;
+
+        this.settingsManager = new SettingsManager<>(
+                pluginFileSystem,
+                pluginId
+        );
+
+        return this.settingsManager;
+    }
+
+    @Override
+    public ActiveActorIdentityInformation getSelectedActorIdentity() throws CantGetSelectedActorIdentityException, ActorIdentityNotSelectedException {
+        return null;
+    }
+
+    @Override
+    public void createIdentity(String name, String phrase, byte[] profile_img) throws Exception {
+
+    }
+
+    @Override
+    public void setAppPublicKey(String publicKey) {
+
+    }
+
+    @Override
+    public int[] getMenuNotifications() {
+        return new int[0];
     }
 }
